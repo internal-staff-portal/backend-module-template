00dba889584c9c448046b04342b3755cab536952
Laurenz1606 <laurenz.rausche@gmail.com>
2022-03-12 20:08:06 +0100

    add socket.io support

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,21 @@ interface ModuleOptions {}
 export default function ModuleWrapper(options: ModuleOptions): ModuleConstructor {
   //the constructor
   return function (core: CoreValues): Module {
-    //the router
+    //define module path
+    const path = "/TModule";
+
+    //create the router
     const TModuleRouter = Router()
+
+    //create the socket.io namespace 
+    const namespace = core.createNamespace(path)
 
     //the module code here
 
     //return the actual module
     return {
       name: "TModule",
-      path: "/TModule", 
+      path: path,
       router: TModuleRouter
     }
   };
